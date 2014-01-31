86430fe13942593ca782edc909cb253bc4e0bb87
Ben Grawi <onionhouseprod@gmail.com>
2014-01-31 13:45:05 -0500

    More set-up - Put css in its own file - New grunt config - Some chart experimentation

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,17 +2,29 @@ module.exports = function(grunt) {
 
   // Project configuration.
 	grunt.initConfig({
+		common: {
+			banner: '/*! <%= pkg.name %> <%= pkg.version %> <%= grunt.template.today("yyyy-mm-dd") %> License: MIT */'
+		},
 		pkg: grunt.file.readJSON('package.json'),
 		clean: {
 			build: ["build"],
 		},
 		uglify: {
 			options: {
-				banner: '/*! <%= pkg.name %> <%= pkg.version %> <%= grunt.template.today("yyyy-mm-dd") %> License: MIT */\n'
+				banner: '<%=common.banner%>\n'
 			},
 			build: {
 				src: 'src/<%= pkg.name %>.js',
 				dest: 'build/<%= pkg.name %>.min.js'
+			}
+		},
+		cssmin: {
+			options: {
+				banner: '<%=common.banner%>'
+			},
+			minify: {
+				src: 'src/minChart.css',
+				dest: 'build/minChart.min.css',
 			}
 		},
 		watch: {
@@ -32,13 +44,14 @@ module.exports = function(grunt) {
 
   // Load Plugins
   grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
   grunt.loadNpmTasks('grunt-contrib-connect');
   grunt.loadNpmTasks('grunt-contrib-clean');
   grunt.loadNpmTasks('grunt-contrib-watch');
 
   // Register all the task(s).
   
-  grunt.registerTask('default', ['clean:build', 'uglify']);
+  grunt.registerTask('default', ['clean:build', 'uglify', 'cssmin:minify']);
   
   grunt.registerTask('server', ['connect']);
 
--- src/minChart.js
+++ src/minChart.js
@@ -12,10 +12,18 @@
 	minChart.directive("mcPie", function() {
 		return {
 			scope: {
-				data: '=mcData'
+				data: '='
 			},
 			restrict: 'E',
-			template: '<div><pre>{{data | json}}</pre> mcPie initializted</div>'
+			template: '<div class="clearfix mc-chart mc-pie"><div class="mc-sections"><div class="sect" data-start="0" data-value="30"></div>' +
+				'<div class="sect highlight" data-start="30" data-value="30"></div>' +
+				'<div class="sect" data-start="60" data-value="40"></div>' +
+				'<div class="sect big" data-start="100" data-value="260"></div></div>',
+			link: {
+				pre: function(scope, elm, attrs) {
+					
+				}
+			}
 		};
 	});
 })(window, angular);
